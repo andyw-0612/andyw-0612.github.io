ceed616ffd45c7775e26686c137018fe51cf93c5
andyw-0612 <andyw0612@gmail.com>
2022-12-18 13:21:49 -0800

    initial local commit

---
+++ _notes/move your body every day.md
@@ -0,0 +1,16 @@
+---
+desc: "this is a note about moving your bodies"
+has_parent: "First Note"
+---
+## Heading Number One 
+
+Move your body every day. Benefits include:
+
+$$ x = some random math$$
+
+- Improved sleep quality
+- Less risk of chronic disease
+- Increased productivity
+- Reduced anxiety
+
+The "every day" part is important, because [[consistency]] is key to most things worth doing.
---
+++ _plugins/obsidian_markdown.rb
@@ -0,0 +1,41 @@
+class ObsidianMarkdown < Jekyll::Generator
+    def generate(site)
+        all_notes = site.collections['notes'].docs
+        all_pages = site.pages
+    
+        all_docs = all_notes + all_pages
+    
+        link_extension = !!site.config["use_html_extension"] ? '.html' : ''
+        callout(all_docs)
+        
+    end
+
+    def callout(doc)
+        # convert obsidian callouts like "[!Info]" to display as callouts in html
+        for current_note in doc do
+            # Capture everything starting with ">" and ending with a newline that does not start with ">"
+            current_note.content.gsub!(/^>(.*?)\n(?!>)/m) do |matched_text|
+                # Check if the first line starts with ">[!" and "]"
+                if matched_text =~ /^>\s*\[!(.*?)\]\s*$/
+                    # Replace the first line with the text between "[!" and "]" wrapped in div tags
+                    matched_text.sub!(/^>\s*\[!(.*?)\]\s*\n/, "> <div class='callout-title'>#{$1}</div> \n")
+                    # Add "{: .callout}" to the end of the matched text
+                    matched_text << ("{: .callout}")
+                
+                    # Add a new line at the beginning and end of the matched text
+                    matched_text.prepend("\n")
+                    matched_text << "\n"
+                    puts matched_text        
+
+
+                end
+            
+                # Return the modified matched text
+                matched_text
+            end
+        end
+
+    end
+      
+    
+end
